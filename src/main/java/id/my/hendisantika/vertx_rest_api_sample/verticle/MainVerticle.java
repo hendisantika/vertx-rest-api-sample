b42a9b4f5b584449cc07418bc830dbf895988ca7
hendisantika <hendisantika@yahoo.co.id>
2025-01-11 06:35:23 +0700

    Add deployMigrationVerticle in MainVerticle🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_rest_api_sample/verticle/MainVerticle.java
+++ src/main/java/id/my/hendisantika/vertx_rest_api_sample/verticle/MainVerticle.java
@@ -2,6 +2,9 @@ package id.my.hendisantika.vertx_rest_api_sample.verticle;
 
 import id.my.hendisantika.vertx_rest_api_sample.util.LogUtils;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 
@@ -17,5 +20,16 @@ public class MainVerticle extends AbstractVerticle {
       .flatMap(migrationVerticleId -> deployApiVerticle(vertx))
       .onSuccess(success -> LOGGER.info(LogUtils.RUN_APP_SUCCESSFULLY_MESSAGE.buildMessage(System.currentTimeMillis() - start)))
       .onFailure(throwable -> LOGGER.error(throwable.getMessage()));
+  }
+
+  private Future<Void> deployMigrationVerticle(Vertx vertx) {
+    final DeploymentOptions options = new DeploymentOptions()
+      .setWorker(true)
+      .setWorkerPoolName("migrations-worker-pool")
+      .setInstances(1)
+      .setWorkerPoolSize(1);
+
+    return vertx.deployVerticle(MigrationVerticle.class.getName(), options)
+      .flatMap(vertx::undeploy);
   }
 }

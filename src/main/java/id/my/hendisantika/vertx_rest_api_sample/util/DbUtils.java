0de3b85280411bc99d4252ec0452499fa4ef8d25
hendisantika <hendisantika@yahoo.co.id>
2025-01-11 06:17:15 +0700

    Add DbUtils🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_rest_api_sample/util/DbUtils.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.vertx_rest_api_sample.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-rest-api-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/01/25
+ * Time: 06.17
+ * To change this template use File | Settings | File Templates.
+ */
+public class DbUtils {
+  private static final String HOST_CONFIG = "datasource.host";
+  private static final String PORT_CONFIG = "datasource.port";
+  private static final String DATABASE_CONFIG = "datasource.database";
+  private static final String USERNAME_CONFIG = "datasource.username";
+  private static final String PASSWORD_CONFIG = "datasource.password";
+
+  private DbUtils() {
+
+  }
+
+}

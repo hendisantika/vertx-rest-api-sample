e75a67d9f247b30669043565057fc859ae158796
hendisantika <hendisantika@yahoo.co.id>
2025-01-11 06:30:21 +0700

    Add buildBookRouter in BookRouter🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_rest_api_sample/api/router/BookRouter.java
+++ src/main/java/id/my/hendisantika/vertx_rest_api_sample/api/router/BookRouter.java
@@ -3,6 +3,9 @@ package id.my.hendisantika.vertx_rest_api_sample.api.router;
 import id.my.hendisantika.vertx_rest_api_sample.api.handler.BookHandler;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.LoggerFormat;
+import io.vertx.ext.web.handler.LoggerHandler;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +29,17 @@ public class BookRouter {
 
   public void setRouter(Router router) {
     router.mountSubRouter("/api/v1", buildBookRouter());
+  }
+
+  private Router buildBookRouter() {
+    final Router bookRouter = Router.router(vertx);
+    bookRouter.route("/books*").handler(BodyHandler.create());
+    bookRouter.get("/books").handler(LoggerHandler.create(LoggerFormat.DEFAULT)).handler(bookHandler::readAll);
+    bookRouter.get("/books/:id").handler(LoggerHandler.create(LoggerFormat.DEFAULT)).handler(bookHandler::readOne);
+    bookRouter.post("/books").handler(LoggerHandler.create(LoggerFormat.DEFAULT)).handler(bookHandler::create);
+    bookRouter.put("/books/:id").handler(LoggerHandler.create(LoggerFormat.DEFAULT)).handler(bookHandler::update);
+    bookRouter.delete("/books/:id").handler(LoggerHandler.create(LoggerFormat.DEFAULT)).handler(bookHandler::delete);
+
+    return bookRouter;
   }
 }

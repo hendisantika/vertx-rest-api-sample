ac6b8f8d6afd36fe4b842751166ac4472df35112
hendisantika <hendisantika@yahoo.co.id>
2025-01-11 06:19:28 +0700

    Add ResponseUtils🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_rest_api_sample/util/ResponseUtils.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.vertx_rest_api_sample.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-rest-api-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/01/25
+ * Time: 06.19
+ * To change this template use File | Settings | File Templates.
+ */
+public class ResponseUtils {
+  private static final String CONTENT_TYPE_HEADER = "Content-Type";
+  private static final String APPLICATION_JSON = "application/json";
+
+  private ResponseUtils() {
+
+  }
+}

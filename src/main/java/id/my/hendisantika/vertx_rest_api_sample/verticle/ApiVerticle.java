737e93e227344ea3b7e9a3b59a4e97558377b8a8
hendisantika <hendisantika@yahoo.co.id>
2025-01-11 06:33:38 +0700

    Add ApiVerticle🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_rest_api_sample/verticle/ApiVerticle.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.vertx_rest_api_sample.verticle;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-rest-api-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/01/25
+ * Time: 06.32
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApiVerticle extends AbstractVerticle {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ApiVerticle.class);
+}

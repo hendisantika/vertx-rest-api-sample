17ab19bfd76d799e69036cb4b99d5817783822a5
hendisantika <hendisantika@yahoo.co.id>
2025-01-11 06:17:35 +0700

    Add buildDbClient in DbUtils🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_rest_api_sample/util/DbUtils.java
+++ src/main/java/id/my/hendisantika/vertx_rest_api_sample/util/DbUtils.java
@@ -1,5 +1,11 @@
 package id.my.hendisantika.vertx_rest_api_sample.util;
 
+import io.vertx.core.Vertx;
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.pgclient.PgPool;
+
+import java.util.Properties;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : vertx-rest-api-sample
@@ -18,7 +24,21 @@ public class DbUtils {
   private static final String PASSWORD_CONFIG = "datasource.password";
 
   private DbUtils() {
+  }
 
+  public static PgPool buildDbClient(Vertx vertx) {
+    final Properties properties = ConfigUtils.getInstance().getProperties();
+
+    final PgConnectOptions connectOptions = new PgConnectOptions()
+      .setPort(Integer.parseInt(properties.getProperty(PORT_CONFIG)))
+      .setHost(properties.getProperty(HOST_CONFIG))
+      .setDatabase(properties.getProperty(DATABASE_CONFIG))
+      .setUser(properties.getProperty(USERNAME_CONFIG))
+      .setPassword(properties.getProperty(PASSWORD_CONFIG));
+
+    final PoolOptions poolOptions = new PoolOptions().setMaxSize(ApplicationUtils.numberOfAvailableCores());
+
+    return PgPool.pool(vertx, connectOptions, poolOptions);
   }
 
 }

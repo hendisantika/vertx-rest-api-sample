feace6efbbadf6efeefafa54ad56389c31f49a05
hendisantika <hendisantika@yahoo.co.id>
2025-01-11 06:32:40 +0700

    Add delete in BookService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_rest_api_sample/api/service/BookService.java
+++ src/main/java/id/my/hendisantika/vertx_rest_api_sample/api/service/BookService.java
@@ -90,4 +90,13 @@ public class BookService {
       .onSuccess(success -> LOGGER.info(LogUtils.REGULAR_CALL_SUCCESS_MESSAGE.buildMessage("Update one book", success)))
       .onFailure(throwable -> LOGGER.error(LogUtils.REGULAR_CALL_ERROR_MESSAGE.buildMessage("Update one book", throwable.getMessage())));
   }
+
+  public Future<Void> delete(Integer id) {
+    return dbClient.withTransaction(
+        connection -> {
+          return bookRepository.delete(connection, id);
+        })
+      .onSuccess(success -> LOGGER.info(LogUtils.REGULAR_CALL_SUCCESS_MESSAGE.buildMessage("Delete one book", id)))
+      .onFailure(throwable -> LOGGER.error(LogUtils.REGULAR_CALL_ERROR_MESSAGE.buildMessage("Delete one book", throwable.getMessage())));
+  }
 }
